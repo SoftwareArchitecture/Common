3494fe02490b441cfe516cccbe2b41161edcdced
Amin Mirdamadi <amin.mtehrani@gmail.com>
2014-01-10 21:20:27 +0100

    update PeerInfo

--- src/main/java/at/ac/tuwien/softwarearchitecture/swazam/common/infos/PeerInfo.java
+++ src/main/java/at/ac/tuwien/softwarearchitecture/swazam/common/infos/PeerInfo.java
@@ -33,6 +33,28 @@ public class PeerInfo implements Serializable{
 	 */
     @XmlAttribute(name = "port", required = true)
 	private int port;
+    
+	@XmlAttribute(name = "username")
+	private String username;
+	
+	@XmlAttribute(name = "password")
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public PeerInfo() {
 		super();
