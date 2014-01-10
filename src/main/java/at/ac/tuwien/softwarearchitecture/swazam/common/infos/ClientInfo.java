9626ae09ab3d8e8fc220e1e4c362e1311d912c9d
Amin Mirdamadi <amin.mtehrani@gmail.com>
2014-01-10 16:30:39 +0100

    update ClientInfo with username passwird attributes

--- src/main/java/at/ac/tuwien/softwarearchitecture/swazam/common/infos/ClientInfo.java
+++ src/main/java/at/ac/tuwien/softwarearchitecture/swazam/common/infos/ClientInfo.java
@@ -28,6 +28,22 @@ public class ClientInfo implements Serializable{
 	private String password;
 	
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public UUID getSessionKey() {
 		return sessionKey;
 	}
